1f54ebdbb65f9b354d734e862463850e3f2801af
Adam Placzek <adam.placzek@nokia.com>
2018-04-04 14:07:05 +0200

    Added some things

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -2,4 +2,8 @@
 set -e
 
 (cd $KONG_DOWNLOAD; bin/busted -c -v $TRAVIS_BUILD_DIR/spec)
+(cd $KONG_DOWNLOAD; pwd; ls -la)
+(pwd; ls -la)
+(cd $TRAVIS_BUILD_DIR; ls -la)
+(cd $TRAVIS_BUILD_DIR/spec; ls -la)
 (cd $KONG_DOWNLOAD; luacov-coveralls)
