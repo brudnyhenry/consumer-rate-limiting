a188197ecf284af08e0b041394e3df664de0132b
Adam Placzek <adam.placzek@nokia.com>
2018-04-04 15:04:06 +0200

    Added luacov

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -2,8 +2,9 @@
 set -e
 
 (cd  $KONG_DOWNLOAD; bin/busted --verbose --coverage $TRAVIS_BUILD_DIR/spec)
+(cd $KONG_DOWNLOAD/bin; pwd; ls -la)
 (cd $KONG_DOWNLOAD; pwd; ls -la)
 (pwd; ls -la)
-(cd $TRAVIS_BUILD_DIR; ls -la)
-(cd $TRAVIS_BUILD_DIR/spec; ls -la)
-(cd $KONG_DOWNLOAD; luacov-coveralls)
+(cd $TRAVIS_BUILD_DIR;pwd; ls -la)
+(cd $TRAVIS_BUILD_DIR/spec/consumer-rate-limiting;pwd; ls -la)
+luacov-coveralls
