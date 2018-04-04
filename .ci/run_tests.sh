7c6acd51b6f5a15a0e2c74f57ed40324d17f2199
Adam Placzek <adam.placzek@nokia.com>
2018-04-04 14:48:33 +0200

    Added luacov

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-(cd $TRAVIS_BUILD_DIR; $KONG_DOWNLOAD/bin/busted -c -v $TRAVIS_BUILD_DIR/spec)
+(cd  $KONG_DOWNLOAD; bin/busted --verbose --coverage $TRAVIS_BUILD_DIR/spec)
 (cd $KONG_DOWNLOAD; pwd; ls -la)
 (pwd; ls -la)
 (cd $TRAVIS_BUILD_DIR; ls -la)
